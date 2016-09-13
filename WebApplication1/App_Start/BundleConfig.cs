1981e788f9f1a0cff5efc7b21c419f752bef43a2
mln6666 <poncemarioleo@gmail.com>
2016-09-13 14:30:26 -0300

    +Listado de PS ahora tiene autoordenamiento, busqueda,paginas,etc

--- WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -8,10 +8,19 @@ namespace WebApplication1
         // Para obtener más información sobre Bundles, visite http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-1.12.4.js",
                         "~/Scripts/jquery-1.12.4.min.js",
-                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+                        "~/Scripts/jquery.unobtrusive-ajax.js", "~/Scripts/bootstrap.js",
+                        "~/Scripts/datatables/jquery.datatables.js",
+                      "~/Scripts/datatables/datatables.bootstrap.js",
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootbox.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+            //          "~/Scripts/datatables/jquery.datatables.js",
+            //          "~/Scripts/datatables/datatables.bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -21,14 +30,15 @@ namespace WebApplication1
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootstrap.min.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/bootbox.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/bootstrap.min.js",
+            //          "~/Scripts/respond.js",
+            //          "~/Scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
