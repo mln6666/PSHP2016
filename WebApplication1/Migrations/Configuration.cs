3de8e680423a7c7c4aa4fe7093b43fc42d5461c8
gabsfachinotti <gabsfachinotti@gmail.com>
2016-09-24 18:30:01 -0300

    Para superUsuario
    Se crearon los siguientes roles: CrearAlumno, CrearPS, CrearPlan, CrearInforme, CrearArea, CrearOrganizacion.
    
    Se crearon los siguientes roles: EditarAlumno, EditarPS, EditarPlan, EditarInforme, EditarArea, EditarOrganizacion
    
    Se crearon los siguientes roles: EliminarAlumno, EliminarPS, EliminarPlan, EliminarInforme, EliminarArea, EliminarOrganizacion

--- WebApplication1/Migrations/Configuration.cs
+++ WebApplication1/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace WebApplication1.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "WebApplication1.Context.ContextPS";
         }
 
