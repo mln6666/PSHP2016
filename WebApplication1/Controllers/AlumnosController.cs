db87f185555355490a300230732800c1a8d891a8
mln6666 <poncemarioleo@gmail.com>
2016-09-25 18:20:05 -0300

    +PA GABO

--- WebApplication1/Controllers/AlumnosController.cs
+++ WebApplication1/Controllers/AlumnosController.cs
@@ -20,8 +20,8 @@ namespace WebApplication1.Controllers
         {
 
             var pss = (   from p in db.PSs
-                           where p.Estado == Estado.PS_Aprobada | p.Estado == Estado.PS_Cancelada 
-                           | p.Estado == Estado.PS_Vencida | p.Estado == Estado.Plan_Rechazado
+                           where p.Estado == Estado.PS_Aprobada /*| p.Estado == Estado.PS_Cancelada */
+                           | p.Estado == Estado.PS_Vencida /*| p.Estado == Estado.Plan_Rechazado*/
                            select p);
 
             var alumnos = db.Alumnos.ToList();
