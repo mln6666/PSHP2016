dfb171abbf1a424d3c4892b0d7eeb5f889bd735e
mln6666 <poncemarioleo@gmail.com>
2016-10-06 12:57:06 -0300

    ++++ ++++ ++++ ++++

--- WebApplication1/Controllers/EstadisticasController.cs
+++ WebApplication1/Controllers/EstadisticasController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using WebApplication1.Context;
 using WebApplication1.Models;
 
@@ -13,7 +14,7 @@ namespace WebApplication1.Controllers
         // GET: Estadisticas
         private ContextPS db = new ContextPS();
 
-        [Authorize(Roles = "Moderador,Invitado,Administrador")]
+        [Authorize(Roles = "Moderador,Administrador")]
         public ActionResult Index()
         {
 
@@ -22,7 +23,7 @@ namespace WebApplication1.Controllers
 
 
 
-        [Authorize(Roles = "Moderador,Invitado,Administrador")]
+        [Authorize(Roles = "Moderador,Administrador")]
         [HttpPost]
         public ActionResult EstadosPlanes(DateTime? fecha1,DateTime? fecha2)
         {
@@ -38,7 +39,7 @@ namespace WebApplication1.Controllers
 
             foreach (var item in db.PresentacionPlanes)
             {
-                if(item.FechaEvaluacionPlan>= fecha1 & item.FechaEvaluacionPlan<= fecha2)
+                if(item.FechaPresentacionPlan>= fecha1 & item.FechaPresentacionPlan<= fecha2)
                 { 
                 if (item.EstadoEvaluacionPlan == Evaluacion.Aprobado)
                 {
@@ -56,12 +57,9 @@ namespace WebApplication1.Controllers
                 {
                         planespendientes++;
                 }
-
+               
                 }
-                if (item.FechaEvaluacionPlan == null & item.EstadoEvaluacionPlan == Evaluacion.Pendiente)
-                {
-                        planespendientes++;
-                }
+               
 
 
             }
@@ -71,6 +69,275 @@ namespace WebApplication1.Controllers
             ViewBag.planesdesaprobados = planesdesaprobados;
             ViewBag.planesrechazados = planesrechazados;
             ViewBag.planespendientes = planespendientes;
+            return View();
+        }
+        [Authorize(Roles = "Moderador,Administrador")]
+        [HttpPost]
+        public ActionResult EstadosInformes(DateTime? fecha1, DateTime? fecha2)
+        {
+            //fecha1 = Convert.ToDateTime("01/02/0001").Date;
+
+            //fecha2 = Convert.ToDateTime("01/02/2200").Date;
+
+
+            var informespendientes = 0;
+            var informesaprobados = 0;
+            var informesdesaprobados = 0;
+
+            foreach (var item in db.PresentacionInformes)
+            {
+                if (item.FechaPresentacionInforme >= fecha1 & item.FechaPresentacionInforme <= fecha2)
+                {
+                    if (item.EstadoEvaluacionInforme == Evaluacion.Aprobado)
+                    {
+                        informesaprobados++;
+                    }
+                    if (item.EstadoEvaluacionInforme == Evaluacion.Desaprobado)
+                    {
+                        informesdesaprobados++;
+                    }
+                    
+                    if (item.EstadoEvaluacionInforme == Evaluacion.Pendiente)
+                    {
+                        informespendientes++;
+                    }
+
+                }
+
+
+
+            }
+            ViewBag.fechadesde = fecha1;
+            ViewBag.fechahasta = fecha2;
+            ViewBag.informesaprobados = informesaprobados;
+            ViewBag.informesdesaprobados = informesdesaprobados;
+            ViewBag.informespendientes = informespendientes;
+            return View();
+        }
+
+
+        [Authorize(Roles = "Moderador,Administrador")]
+        [HttpPost]
+        public ActionResult AreasPlanes(DateTime? fecha1, DateTime? fecha2)
+        {
+            //fecha1 = Convert.ToDateTime("01/02/0001").Date;
+
+            //fecha2 = Convert.ToDateTime("01/02/2200").Date;
+            var listaareas = db.Areas.ToList();
+
+            List<string> misareas = new List<string>();
+            List<int> cantidades = new List<int>();
+
+            foreach (var item in listaareas)
+            {
+                misareas.Add(item.NombreArea);
+            }
+            int total = 0;
+
+            for (int i = 0;i< listaareas.Count(); i++)
+            {
+            foreach (var item in db.PresentacionPlanes)
+            {
+                if (item.FechaPresentacionPlan >= fecha1 & item.FechaPresentacionPlan <= fecha2)
+                {
+                    if (item.PS.Area.NombreArea == listaareas.ElementAt(i).NombreArea)
+                    {
+                        total++;
+                    }
+
+                }
+            }
+                cantidades.Add(total);
+                total = 0;
+            }
+            ViewBag.fechadesde = fecha1;
+            ViewBag.fechahasta = fecha2;
+            ViewBag.listaareas = JsonConvert.SerializeObject(misareas);
+            ViewBag.cantidades = JsonConvert.SerializeObject(cantidades);
+
+           
+
+            return View();
+        }
+
+        [Authorize(Roles = "Moderador,Administrador")]
+        [HttpPost]
+        public ActionResult AreasInformes(DateTime? fecha1, DateTime? fecha2)
+        {
+            //fecha1 = Convert.ToDateTime("01/02/0001").Date;
+
+            //fecha2 = Convert.ToDateTime("01/02/2200").Date;
+            var listaareas = db.Areas.ToList();
+
+            List<string> misareas = new List<string>();
+            List<int> cantidades = new List<int>();
+
+            foreach (var item in listaareas)
+            {
+                misareas.Add(item.NombreArea);
+            }
+            int total = 0;
+
+            for (int i = 0; i < listaareas.Count(); i++)
+            {
+                foreach (var item in db.PresentacionInformes)
+                {
+                    if (item.FechaPresentacionInforme >= fecha1 & item.FechaPresentacionInforme <= fecha2)
+                    {
+                        if (item.PS.Area.NombreArea == listaareas.ElementAt(i).NombreArea)
+                        {
+                            total++;
+                        }
+
+                    }
+                }
+                cantidades.Add(total);
+                total = 0;
+            }
+            ViewBag.fechadesde = fecha1;
+            ViewBag.fechahasta = fecha2;
+            ViewBag.listaareas = JsonConvert.SerializeObject(misareas);
+            ViewBag.cantidades = JsonConvert.SerializeObject(cantidades);
+
+
+
+            return View();
+        }
+
+
+        [Authorize(Roles = "Moderador,Administrador")]
+        [HttpPost]
+        public ActionResult TipospsPlanes(DateTime? fecha1, DateTime? fecha2)
+        {
+            //fecha1 = Convert.ToDateTime("01/02/0001").Date;
+
+            //fecha2 = Convert.ToDateTime("01/02/2200").Date;
+            var listatiposps = db.TipoPSs.ToList();
+
+            List<string> mistiposps = new List<string>();
+            List<int> cantidades = new List<int>();
+
+            foreach (var item in listatiposps)
+            {
+                mistiposps.Add(item.NombreTipoPS);
+            }
+            int total = 0;
+
+            for (int i = 0; i < listatiposps.Count(); i++)
+            {
+                foreach (var item in db.PresentacionPlanes)
+                {
+                    if (item.FechaPresentacionPlan >= fecha1 & item.FechaPresentacionPlan <= fecha2)
+                    {
+                        if (item.PS.TipoPS.NombreTipoPS == listatiposps.ElementAt(i).NombreTipoPS)
+                        {
+                            total++;
+                        }
+
+                    }
+                }
+                cantidades.Add(total);
+                total = 0;
+            }
+            ViewBag.fechadesde = fecha1;
+            ViewBag.fechahasta = fecha2;
+            ViewBag.listatiposps = JsonConvert.SerializeObject(mistiposps);
+            ViewBag.cantidades = JsonConvert.SerializeObject(cantidades);
+
+
+
+            return View();
+        }
+
+        [Authorize(Roles = "Moderador,Administrador")]
+        [HttpPost]
+        public ActionResult TipospsInformes(DateTime? fecha1, DateTime? fecha2)
+        {
+            //fecha1 = Convert.ToDateTime("01/02/0001").Date;
+
+            //fecha2 = Convert.ToDateTime("01/02/2200").Date;
+            var listatiposps = db.TipoPSs.ToList();
+
+            List<string> mistiposps = new List<string>();
+            List<int> cantidades = new List<int>();
+
+            foreach (var item in listatiposps)
+            {
+                mistiposps.Add(item.NombreTipoPS);
+            }
+            int total = 0;
+
+            for (int i = 0; i < listatiposps.Count(); i++)
+            {
+                foreach (var item in db.PresentacionInformes)
+                {
+                    if (item.FechaPresentacionInforme >= fecha1 & item.FechaPresentacionInforme <= fecha2)
+                    {
+                        if (item.PS.TipoPS.NombreTipoPS == listatiposps.ElementAt(i).NombreTipoPS)
+                        {
+                            total++;
+                        }
+
+                    }
+                }
+                cantidades.Add(total);
+                total = 0;
+            }
+            ViewBag.fechadesde = fecha1;
+            ViewBag.fechahasta = fecha2;
+            ViewBag.listatiposps = JsonConvert.SerializeObject(mistiposps);
+            ViewBag.cantidades = JsonConvert.SerializeObject(cantidades);
+
+
+
+            return View();
+        }
+
+        [Authorize(Roles = "Moderador,Administrador")]
+        //[HttpPost]
+        public ActionResult AñoingresoPS(int fecha1, int fecha2)
+        {
+            
+
+            List<int> misaños = new List<int>();
+            List<int> cantidades = new List<int>();
+            List<int> cantidadesps = new List<int>();
+
+
+
+            int años = fecha2 - fecha1;
+
+            for (int i=0;i<años+1;i++)
+            {
+                misaños.Add(fecha1+i);
+            }
+            int total = 0;
+            int total1 = 0;
+
+            for (int i = 0; i < misaños.Count(); i++)
+            {
+                foreach (var item in db.Alumnos)
+                {
+                    if (item.AñoIngreso == misaños[i])
+                    {
+                            total++;
+                        total1 += item.PSs.Count();
+                    }
+                }
+                cantidades.Add(total);
+                cantidadesps.Add(total1);
+                total = 0;
+                total1 = 0;
+            }
+            ViewBag.fechadesde = fecha1;
+            ViewBag.fechahasta = fecha2;
+            ViewBag.listaaños = JsonConvert.SerializeObject(misaños);
+            ViewBag.cantidades = JsonConvert.SerializeObject(cantidades);
+            ViewBag.cantidadesps = JsonConvert.SerializeObject(cantidadesps);
+
+
+
+
             return View();
         }
 
