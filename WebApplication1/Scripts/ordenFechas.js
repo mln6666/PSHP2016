21459946b7295cfd27c254f15ee79bc2cdf0b6d2
mln6666 <poncemarioleo@gmail.com>
2016-10-23 17:34:25 -0300

    +Cambiados los paneles por tablas en todas las estadisticas de Planes/Informes +Agregadas validaciones de Campos obligatorios y de Max/Min Fecha en todas las estadisticas

--- WebApplication1/Scripts/ordenFechas.js
+++ WebApplication1/Scripts/ordenFechas.js
@@ -1,8 +1,10 @@
-﻿ function ordenFechas() {
-        if ($('#from').val().trim() >= $('#to').val().trim() ) {
+﻿function ordenFechas() {
+    if( ($('#from').val()!="") &($('#to').val()!="") ){
+        if ($('#from').val().trim() > $('#to').val().trim() ) {
             $('#spanOrdenFechas').text("Rango de fechas incorrecto, la fecha de inicio supera a la de fin.");
             event.preventDefault();
         }else{
             $('#spanOrdenFechas').text("");
         }
+    }
     }
